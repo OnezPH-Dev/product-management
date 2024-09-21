29bbcf2d8eda010ef10367f0a26aa1f825794a68
Wilzen Carl R. Tolentino <onez.ph.dev@gmail.com>
2024-09-21 07:40:14 -0700

    proment

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,10 @@
 import './bootstrap';
 
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp, Head, Link } from '@inertiajs/vue3'
 
 createInertiaApp({
+  titll: (title) => `Proment ${title}`,
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     return pages[`./Pages/${name}.vue`]
@@ -12,6 +13,8 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .component('Head', Head)
+      .component('Link', Link)
       .mount(el)
   },
 })
